6a4632bb344b0bc221c2fc1a68ac68c7f143b686
unknown <alxmer111@gmail.com>
2020-02-23 19:22:32 +0400

    analysed type, waitgroup

--- main.go
+++ main.go
@@ -9,13 +9,19 @@ import (
 	"net/url"
 	"os"
 	"strings"
+	"sync"
 )
 
 const (
 	searchWord = "Go"
 )
 
-// isValidUrl tests a string to determine if it is a well-structured url or not.
+type analysed struct {
+	url   string
+	count int
+}
+
+// isValidUrl tests a string to determine if it is a well-structured URL or not.
 func isValidURL(toTest string) bool {
 	_, err := url.ParseRequestURI(toTest)
 	if err != nil {
@@ -30,9 +36,8 @@ func isValidURL(toTest string) bool {
 	return true
 }
 
-//searchInURL counts the number of search word
-//in a given URL
-func searchInURL(url string) int {
+// searchInURL counts the number of search word in a given URL.
+func searchInURL(url string) analysed {
 	if !isValidURL(url) {
 		log.Fatal("Error! Not valid url format!")
 	}
@@ -49,21 +54,28 @@ func searchInURL(url string) int {
 		log.Fatal(err)
 	}
 
-	return strings.Count(string(body), searchWord)
+	return analysed{
+		url,
+		strings.Count(string(body), searchWord),
+	}
 }
 
-func printResponse(ch chan int, total *int) {
+// printResponse prints the counter for each URL and updates the total counter.
+func printResponse(ch <-chan analysed, total *int) {
 	for c := range ch {
-		*total += c
-		fmt.Printf("Count for test %d\n", c)
+		*total += c.count
+		fmt.Printf("Count for %s: %d\n", c.url, c.count)
 	}
 }
 
 func main() {
 	var total int
-	fmt.Println("Enter valid urls using space as delimeter")
+	wg := new(sync.WaitGroup)
+	urlChan := make(chan analysed)
 
-	urlChan := make(chan int)
+	fmt.Println("Enter valid urls using space as delimeter")
+	fmt.Println("Type 'quit' or tap Ctrl+C to stop and see the total counts")
+
 	go printResponse(urlChan, &total)
 
 	scanner := bufio.NewScanner(os.Stdin)
@@ -71,7 +83,16 @@ func main() {
 
 	for scanner.Scan() {
 		text := scanner.Text()
-		urlChan <- searchInURL(text)
+		if text == "quit" {
+			break
+		}
+		wg.Add(1)
+		go func(url string, wg *sync.WaitGroup) {
+			urlChan <- searchInURL(url)
+			wg.Done()
+		}(text, wg)
 	}
+	wg.Wait()
+	close(urlChan)
 	fmt.Println("Total:", total)
 }
